a0d8ec1168c613e3363cd638a23ea80c4934ac29
Stefan Temelkovski <stef.temelkovski@gmail.com>
2022-04-29 11:58:59 +1000

    Modularised navigation and footer

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Insert nav bar at top of all pages
+$(function() {
+    $(".nav-bar").load("navigation.html");
+});
+
+// Insert footer at bottom of all pages
+$(function() {
+    $(".footer").load("footer.html");
+});
