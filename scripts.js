480649dd90ac18ee12b2adbf697057fcb0cc7b97
Stefan Temelkovski <stef.temelkovski@gmail.com>
2022-05-03 11:24:01 +1000

    Attributed code

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 // Insert nav bar at top of all pages
+// modified from https://syntaxfix.com/question/4881/how-can-i-reuse-a-navigation-bar-on-multiple-pages
 $(function() {
     $(".nav-bar").load("navigation.html");
 });
