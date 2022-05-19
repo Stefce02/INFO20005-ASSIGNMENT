c150bcf4701d59afe444c6c37a54bcca7e301172
Stefan Temelkovski <stef.temelkovski@gmail.com>
2022-05-19 13:29:07 +1000

    Toggled cart on every page

--- scripts.js
+++ scripts.js
@@ -9,13 +9,31 @@ $(function() {
     $(".footer").load("footer.html");
 });
 
-// $(function() {
-//     $(".cart").load("cart.html");
-// });
+$(function() {
+    $("#cart").load("cart.html");
+});
 
-// function toggleResponsive {
-//     //   var navbar = document.getElementById("myTopnav");
-//     //   navbar.classList.toggle("myTopnav");
-//     // }
+
+function cartDisplay(){
+    console.log("clicked");
+    var cart = document.getElementById("cart");
+    cart.classList.toggle("cart-shown");
+}
+
+// var cartIcon = document.getElementById("cart-icon");
+
+// if(cartIcon) {
+//     cartIcon.addEventListener("click", display);
+// }
+
+// function display() {
+//     console.log("click");
+// }
+
+
+// cartIcon.addEventListener('click', function handleClick(event) {
+//     console.log("click");
+//     event.target.classList.add('bg-yellow');
+// });
 
 // onclick="newWindow(' + waypointas + ');">
